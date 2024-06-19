deafcc59afe8c8607140c1a3591c2b3a37d4e08c
Nicolas Moncada <nicolas.moncada@hello.wal.agency>
2024-06-19 11:35:22 -0500

    Added specific questions for each person

--- backend/index.js
+++ backend/index.js
@@ -17,27 +17,39 @@ app.use(cors());
 let gameState = {
   players: {},
   questions: [
-    { question: "What is your favourite colour?", type: "individual" },
-    { question: "What is her favourite colour?", type: "matching" },
+    { player1: "What is her favourite colour?", player2: "What is your favourite colour?", display: "What is her favourite colour?" },
+    { player1: "What is her favourite movie?", player2: "What is your favourite movie?", display: "What is her favourite movie?" },
+    { player1: "What is her favourite song?", player2: "What is your favourite song?", display: "What is her favourite song?" },
   ],
+  currentQuestionIndex: 0,
   answers: {},
   scores: { player1: 0, player2: 0 }
+};
+
+const sendQuestion = () => {
+  const question = gameState.questions[gameState.currentQuestionIndex];
+  io.to(gameState.players['player1']).emit('question', { question: question.player1 });
+  io.to(gameState.players['player2']).emit('question', { question: question.player2 });
+  io.emit('displayQuestion', { question: question.display });
 };
 
 io.on('connection', (socket) => {
   console.log('New client connected');
 
   socket.on('joinGame', (player) => {
-    gameState.players[socket.id] = player;
+    gameState.players[player] = socket.id;
     socket.emit('gameState', gameState);
+    if (Object.keys(gameState.players).length === 2) {
+      sendQuestion();
+    }
   });
 
   socket.on('submitAnswer', (data) => {
     const { player, answer } = data;
-    console.log(player, answer)
     gameState.answers[player] = answer;
-    io.emit('updateAnswers', gameState.answers);
-    if (gameState.answers.player1 && gameState.answers.player2) {
+    io.emit('playerAnswered', { player });
+
+    if (Object.keys(gameState.answers).length === 2) {
       if (gameState.answers.player1 === gameState.answers.player2) {
         gameState.scores.player1 += 10;
         gameState.scores.player2 += 10;
@@ -45,13 +57,25 @@ io.on('connection', (socket) => {
         gameState.scores.player1 -= 5;
         gameState.scores.player2 -= 5;
       }
+
       io.emit('updateScores', gameState.scores);
+      gameState.answers = {};
+
+      gameState.currentQuestionIndex++;
+      if (gameState.currentQuestionIndex < gameState.questions.length) {
+        sendQuestion();
+      } else {
+        io.emit('gameOver', gameState.scores);
+      }
     }
   });
 
   socket.on('disconnect', () => {
     console.log('Client disconnected');
-    delete gameState.players[socket.id];
+    const player = Object.keys(gameState.players).find(key => gameState.players[key] === socket.id);
+    if (player) {
+      delete gameState.players[player];
+    }
   });
 });
 
