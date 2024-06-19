2e5d9c0473db47281544dd00b911219751527b97
Nicolas Moncada <nicolas.moncada@hello.wal.agency>
2024-06-19 10:52:58 -0500

    Added backend config test

---
+++ backend/index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+let gameState = {
+  players: {},
+  questions: [
+    { question: "What is your favourite colour?", type: "individual" },
+    { question: "What is her favourite colour?", type: "matching" },
+  ],
+  answers: {},
+  scores: { player1: 0, player2: 0 }
+};
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('joinGame', (player) => {
+    gameState.players[socket.id] = player;
+    socket.emit('gameState', gameState);
+  });
+
+  socket.on('submitAnswer', (data) => {
+    const { player, answer } = data;
+    gameState.answers[player] = answer;
+    io.emit('updateAnswers', gameState.answers);
+    if (gameState.answers.player1 && gameState.answers.player2) {
+      // Compare answers and update scores
+      if (gameState.answers.player1 === gameState.answers.player2) {
+        gameState.scores.player1 += 10;
+        gameState.scores.player2 += 10;
+      } else {
+        gameState.scores.player1 -= 5;
+        gameState.scores.player2 -= 5;
+      }
+      io.emit('updateScores', gameState.scores);
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+    delete gameState.players[socket.id];
+  });
+});
+
+server.listen(4000, () => console.log('Server running on port 4000'));
